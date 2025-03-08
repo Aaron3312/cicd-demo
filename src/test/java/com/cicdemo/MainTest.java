c7738d5f351d84d00bdb650b766a5c0746f52bba
Aaron3312 <a01642529@tec.mx>
2025-03-08 11:14:18 -0600

    cambios

---
+++ src/test/java/com/cicdemo/MainTest.java
@@ -0,0 +1,13 @@
+package com.cicdemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    
+    @Test
+    public void testDummy() {
+        // Simple test that always passes
+        assertTrue(true, "This test should always pass");
+    }
+}
